8ff58cd710217de38ec262e45961e48f5d9e3008
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2025-09-22 16:32:49 -0600

    add PreloaderScene

--- src/game/config.js
+++ src/game/config.js
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
 import { BootScene } from '../scenes/BootScene';
+import { PreloaderScene } from '../scenes/PreloaderScene';
 
 export const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: [BootScene],
+  scene: [BootScene, PreloaderScene],
   parent: 'phaser-game',
 };
